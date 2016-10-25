b01b6d7e7d2ec41f8e6927260beaf2138188de13
Marcin Grygierczyk <kwaska1000@gmail.com>
2016-10-25 12:38:43 +0200

    Init

---
+++ TicTacToe/Engine.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace TicTacToe
+{
+    public enum Mark { X, O }
+
+    class Engine
+    {
+        private Mark currentTurn;
+        private int scoreX, scoreO;
+        private string playerX, playerO;
+        private Mark?[,] board;
+
+        public int ScoreX
+        {
+            get { return scoreX; }
+            private set { scoreX = value; }
+        }
+        public int ScoreO
+        {
+            get { return scoreO; }
+            private set { scoreO = value; }
+        }
+        public string PlayerX { get { return playerX; } }
+        public string PlayerO { get { return playerO; } }
+
+        public Engine()
+        {
+            currentTurn = Mark.X;
+            board = new Mark?[3, 3];
+            ScoreX = ScoreO = 0;
+            playerX = "Player1";
+            playerO = "Player2";
+        }
+
+        private async void ShowMessage(string msg)
+        {
+            MessageDialog msgBox = new MessageDialog(msg);
+            await msgBox.ShowAsync();
+        }
+
+        public void ChangeTurn()
+        {
+            currentTurn = currentTurn == Mark.X ? Mark.O : Mark.X;
+        }
+        public bool Win()
+        {
+            if ((board[0, 0] == Mark.X && board[0, 1] == Mark.X && board[0, 2] == Mark.X) ||
+                (board[0, 0] == Mark.O && board[0, 1] == Mark.O && board[0, 2] == Mark.O) ||
+                (board[1, 0] == Mark.X && board[1, 1] == Mark.X && board[1, 2] == Mark.X) ||
+                (board[1, 0] == Mark.O && board[1, 1] == Mark.O && board[1, 2] == Mark.O) ||
+                (board[2, 0] == Mark.X && board[2, 1] == Mark.X && board[2, 2] == Mark.X) ||
+                (board[2, 0] == Mark.O && board[2, 1] == Mark.O && board[2, 2] == Mark.O) ||
+
+                (board[0, 0] == Mark.X && board[1, 0] == Mark.X && board[2, 0] == Mark.X) ||
+                (board[0, 0] == Mark.O && board[1, 0] == Mark.O && board[2, 0] == Mark.O) ||
+                (board[0, 1] == Mark.X && board[1, 1] == Mark.X && board[2, 1] == Mark.X) ||
+                (board[0, 1] == Mark.O && board[1, 1] == Mark.O && board[2, 1] == Mark.O) ||
+                (board[0, 2] == Mark.X && board[1, 2] == Mark.X && board[2, 2] == Mark.X) ||
+                (board[0, 2] == Mark.O && board[1, 2] == Mark.O && board[2, 2] == Mark.O) ||
+
+                (board[0, 0] == Mark.X && board[1, 1] == Mark.X && board[2, 2] == Mark.X) ||
+                (board[0, 0] == Mark.O && board[1, 1] == Mark.O && board[2, 2] == Mark.O) ||
+                (board[2, 0] == Mark.X && board[1, 1] == Mark.X && board[0, 2] == Mark.X) ||
+                (board[2, 0] == Mark.O && board[1, 1] == Mark.O && board[0, 2] == Mark.O))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public void GameWon()
+        {
+            if (currentTurn == Mark.X)
+            {
+                ShowMessage(playerX + " won! Congratulations!");
+                ScoreX++;
+            }
+            else
+            {
+                ShowMessage(playerO + " won! Congratulations!");
+                ScoreO++;
+            }
+        }
+        public void Draw()
+        {
+            ShowMessage("Tie!");
+        }
+        public void Restart()
+        {
+            Array.Clear(board, 0, board.Length);
+        }
+        public bool PlaceMark(int row, int column, Button sender)
+        {
+            if (row > 2 || row < 0 || column > 2 || column < 0 || board[row, column] != null)
+                return false;
+
+            if (currentTurn == Mark.X)
+            {
+                board[row, column] = Mark.X;
+                sender.Content = "X";
+            }
+            else
+            {
+                board[row, column] = Mark.O;
+                sender.Content = "O";
+            }
+            return true;
+        }
+        public bool IsBoardFilled()
+        {
+            foreach (Mark? field in board)
+            {
+                if (field == null)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ TicTacToe/Game.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Game : Page
+    {
+        private Engine engine;
+
+        public Game()
+        {
+            this.InitializeComponent();
+            engine = new Engine();
+            UpdateScores();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        private void TriggerButtons(bool b)
+        {
+            foreach (object field in grid.Children)
+            {
+                if (field is Button)
+                {
+                    if ((field as Button).Tag != null)
+                        (field as Button).IsEnabled = b;
+                }
+            }
+        }
+        private void CleanBoard()
+        {
+            foreach (object field in grid.Children)
+            {
+                if (field is Button)
+                {
+                    if ((field as Button).Tag != null)
+                        (field as Button).Content = string.Empty;
+                }
+            }
+        }
+        private void UpdateScores()
+        {
+            tbScore1.Text = string.Format("[X] {0}: {1}", engine.PlayerX, engine.ScoreX);
+            tbScore2.Text = string.Format("[O] {0}: {1}", engine.PlayerO, engine.ScoreO);
+        }
+
+        //Events
+        private void FieldClick(object sender, RoutedEventArgs e)
+        {
+            Button field = sender as Button;
+            int row = Convert.ToInt32(field.Name.Substring(5, 1)) - 1;
+            int column = Convert.ToInt32(field.Name.Substring(7, 1)) - 1;
+
+            if (engine.PlaceMark(row, column, field))
+            {
+                if (engine.Win())
+                {
+                    engine.GameWon();
+                    UpdateScores();
+                    TriggerButtons(false);
+                }
+                if(engine.IsBoardFilled())
+                {
+                    engine.Draw();
+                    TriggerButtons(false);
+                }
+                engine.ChangeTurn();
+            }
+        }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            CleanBoard();
+            engine.Restart();
+            TriggerButtons(true);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
